dec6999c1e3ef2a6df24c640577ccb67a6584d79
hadtien <santodaime@outlook.com>
2013-12-22 16:11:05 +0700

    add db_models, move db class definitions to here

---
+++ db_models.py
@@ -0,0 +1,14 @@
+__author__ = 'yura'
+
+from google.appengine.ext import db
+
+
+class User(db.Model):
+    username = db.StringProperty(required=True)
+    password_hash = db.StringProperty(required=True)
+
+
+class WikiPage(db.Model):
+    title = db.StringProperty(required=True)
+    body = db.TextProperty(required=True)
+    #created = db.DateProperty(auto_now_add=True)
